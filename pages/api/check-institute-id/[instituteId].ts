18f3168011afe4e2f964ffd1a683b2ad395b0135
Osama Bin Junaid <32925504+ibnjunaid@users.noreply.github.com>
2022-11-20 23:15:53 +0530

    Custom heading image (#9)
    * removed private files from git
    
    * Added Svg based custom Heading image

--- pages/api/check-institute-id/[instituteId].ts
+++ pages/api/check-institute-id/[instituteId].ts
@@ -3,18 +3,18 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import logger from '../../../utils/logger';
 import { getInstitutions } from '../../../utils/utils';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<string| null>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Object| null>) {
     const instituteId = req.query.instituteId;
     const institutions = await getInstitutions();
     if(institutions !== null){
         if( !institutions.includes(String(instituteId)) ){
-            res.json(`institute Id (${instituteId}) is available.`)
+            res.json({'message': `institute Id (${instituteId}) is available.`, available: true})
         } else {
-            res.json(`institute Id (${instituteId}) taken.`)
+            res.json({'message': `institute Id (${instituteId}) taken.`, available: false})
         }
     } else {
         logger.error('Error in check-institute')
-        res.status(500).send('Idk')
+        res.status(500).json({'message': 'Idk', available: false})
     }
 
 }
