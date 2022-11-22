c17b1f2e4d3f306d89d5333e1f6baa8a23875df0
huzefa bin juned <huzefaghurna@gmail.com>
2022-11-22 18:43:35 +0530

    table.comp Updated

--- components/table.components.tsx
+++ components/table.components.tsx
@@ -10,30 +10,37 @@ export default function Table(props: TableProp) {
   return (
 
     <>
-      <div className="overflow-x-auto">
-        <table className="table w-full">
-          {/* <!-- head --> */}
-          <thead>
-            <tr>
-              <th>Serial</th>
-              {
-                props.headers.map((header, index) => <th key={index}>{header}</th>)
-              }
-            </tr>
-          </thead>
-          <tbody>
-            {
-              props.data.map((entry, i) => {
-                return <tr key={i}>
-                  <th>{i+1}</th>
+      <div className="flex flex-col  sm:p-0 md:p-0 lg:p-0  p-2  m-auto md:w-11/12  lg:text-xl text-xs
+        overflow-hidden   ">
+        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="overflow-hidden">
+              <table className="min-w-full text-center ">
+                {/* <!-- head --> */}
+                <thead >
+                  <tr>
+                    <th>Serial</th>
+                    {
+                      props.headers.map((header, index) => <th key={index}>{header}</th>)
+                    }
+                  </tr>
+                </thead>
+                <tbody>
                   {
-                    entry.map((val, j) => <th key={j}>{val}</th>)
+                    props.data.map((entry, i) => {
+                      return <tr key={i} className='border-solid border-2  border-indigo-200 hover:bg-sky-400 p-2 m-4 cursor-pointer'>
+                        <th >{i + 1}</th>
+                        {
+                          entry.map((val, j) => <th key={j} className=' sm:text-xs md:text-xl  lg:text-xl text-sm'>{val}</th>)
+                        }
+                      </tr>
+                    })
                   }
-                </tr>
-              })
-            }
-          </tbody>
-        </table>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
