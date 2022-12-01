a98c106d60a8197693270d815bdbced7784073d9
huzefa bin juned <huzefaghurna@gmail.com>
2022-12-01 16:36:50 +0530

    changes in table comp...

--- components/table.components.tsx
+++ components/table.components.tsx
@@ -2,8 +2,8 @@ type TableProp = {
   headers: string[];
   data: string[][]
 }
-const colors = [" bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500", " Pastel bg-gradient-to-tr from-violet-400 to-orange-200", " bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-fuchsia-300 via-green-400 to-rose-700"];
-const emogies = ['⭐️⭐️⭐️', '⭐️⭐️', '⭐️']
+const colors = [" bg-gradient-to-r from-rose-100 via-fuchsia-500 to-indigo-500", " Pastel bg-gradient-to-tr from-orange-100 to-violet-500", " bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-fuchsia-100 via-green-400 to-rose-700"];
+const emogies = ['👑', '✨', '💫']
 
 export default function Table(props: TableProp) {
   return (
@@ -31,11 +31,11 @@ export default function Table(props: TableProp) {
                     props.data.map((entry, i) => {
 
                       return <>
-                        <th >
+                        {/* <th >
                           {emogies[i]}
-                        </th>
+                        </th> */}
                         <tr key={i} className={'border-solid border-2  border-indigo-200 hover:bg-sky-400 p-2 m-4 cursor-pointer' + colors[i]} >
-                          <th >{i + 1} </th>
+                          <th >{emogies[i] || i} </th>
                           {
                             entry.map((val, j) => <th key={j} className=' sm:text-xs md:text-xl  lg:text-xl text-sm'>{val}</th>)
                           }
