fa70d3db0f4013f3da248c61b67767ca84cb5d76
ibnjunaid <osamabinjunaid36@gmail.com>
2023-01-06 18:38:52 +0530

    added logging to revalidation logic

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -7,11 +7,12 @@ export default async function handler(
 ) {
     // Check for secret to confirm this is a valid request
     if (req.body.secret !== process.env.TOKEN) {
+        console.log(req.body);
         return res.status(401).json({ message: 'Invalid token' });
     }
 
     try {
-        res.revalidate(`/${req.body.path}`);
+        await res.revalidate(`/${req.body.path}`);
         return res.json({ revalidated: true });
     } catch (err) {
         return res.status(500).json({
